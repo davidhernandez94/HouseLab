40e3fc14bac4808f7cbbacf5c8fac191970c8f49
David Hernandez <6309110@edu.vaniercollege.qc.ca>
2025-10-16 08:41:11 -0400

    added documentation and repo link

--- src/housewithwindowpaneslab/HouseWithWindowPanesLab.java
+++ src/housewithwindowpaneslab/HouseWithWindowPanesLab.java
@@ -16,7 +16,7 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author 6309110
+ * @author david hernandez
  */
 public class HouseWithWindowPanesLab extends Application {
 
@@ -27,42 +27,56 @@ public class HouseWithWindowPanesLab extends Application {
         launch(args);
     }
 
+    /**
+     * main application and logic running
+     * @param stage stage of application
+     */
     @Override
     public void start(Stage stage) {
-        
+        // make a style ground
         Rectangle ground = new Rectangle(0, 700, 800, 100);
         ground.setFill(Color.GREEN);
         
+        // make and style body of house
         Rectangle house = new Rectangle(200, 400, 400, 300);
         house.setStroke(Color.BLACK);
         house.setFill(Color.LIGHTGREY);
         
+        // make and style roof of house
         Polygon roof = new Polygon(200, 400, 600, 400, 400, 250);
         roof.setFill(Color.RED);
         
+        // make and style door
         Rectangle door = new Rectangle(400, 580, 60, 120);
         door.setFill(Color.FIREBRICK);
         
+        // make and style left window
         Rectangle window1 = new Rectangle(240, 480, 90, 90);
         window1.setFill(Color.LIGHTBLUE);
         
+        // make and style right window
         Rectangle window2 = new Rectangle(480, 480, 90, 90);
         window2.setFill(Color.LIGHTBLUE);
         
+        // make windowpanes on windows 
         Line line1 = new Line(285, 480, 285, 570);
         Line line2 = new Line(240, 525, 330, 525);
         Line line3 = new Line(525, 480, 525, 570);
         Line line4 = new Line(480, 525, 570, 525);
         
+        // make and style chimney
         Rectangle chimney = new Rectangle(350, 260, 30, 90);
         chimney.setFill(Color.DARKSLATEGREY);
         
+        // make and style step under house
         Rectangle step = new Rectangle(200, 700, 400, 20);
         step.setFill(Color.BEIGE);
         
+        // make and style sun
         Circle sun = new Circle(650, 100, 70);
         sun.setFill(Color.YELLOW);
         
+        // make and style 4 sun rays
         Line ray1 = new Line(650, 100, 580, 170);
         Line ray2 = new Line(650, 100, 720, 170);
         Line ray3 = new Line(650, 100, 580, 30);
@@ -72,7 +86,7 @@ public class HouseWithWindowPanesLab extends Application {
         ray3.setStroke(Color.YELLOW);
         ray4.setStroke(Color.YELLOW);
         
-        
+        // make pane and add shapes to it
         Pane root = new Pane();
         root.getChildren().add(ground);
         root.getChildren().add(house);
@@ -92,6 +106,7 @@ public class HouseWithWindowPanesLab extends Application {
         root.getChildren().add(ray3);
         root.getChildren().add(ray4);
         
+        // initialize scene and show stage
         Scene scene = new Scene(root, 800, 800);
         stage.setTitle("House with Window Panes");
         stage.setScene(scene);
